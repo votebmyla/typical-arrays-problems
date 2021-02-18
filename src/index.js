ea7168a76c02d22d7ed5b956899b6891afb1022a
votebmyla <votebmyla@gmail.com>
2021-02-18 05:55:27 +0500

    feat: add min max avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  if(array == undefined || array.length == 0){
+    return 0;
+  }else{
+    let minNum = array[0];
+    for(let i=1; i<array.length; i++){
+      if(array[i]<minNum){
+        minNum = array[i];
+      }
+    }
+    return minNum;
+  }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if(array == undefined || array.length == 0){
+    return 0;
+  }else{
+    let maxNum = array[0];
+    for(let i=1; i<array.length; i++){
+      if(array[i]>maxNum){
+        maxNum = array[i];
+      }
+    }
+    return maxNum;
+  }
+
 }
 
 exports.avg = function avg (array) {
+
+  if(array == undefined || array.length == 0){
+    return 0;
+  }else{
+    let sumNum = array[0];
+    for(let i=1; i<array.length; i++){
+      sumNum = sumNum + array[i];
+    }
+
+    let avgNum = sumNum/array.length;
+    return avgNum;
+  }
+
+  
+ 
   return 0;
 }
